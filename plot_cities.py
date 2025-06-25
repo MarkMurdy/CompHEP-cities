a409d474c63d3ad7ec4c05575807b6c59c6118b2
Walker Sundquist <wwsundquist@gmail.com>
2025-06-25 14:10:02 -0400

    Fixes ; Add pville

--- plot_cities.py
+++ plot_cities.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 
-cities = [["Chicago",48, -100],
-          ["Boston", 49, -90],
-          ["Placerville", 39, 121]]
+cities = [["Chicago",41.9, -87.6],
+          ["Boston", 42.4, -71.1],
+          ["Placerville", 38.7, -120.8]]
 scale = 5
 
 map = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
         projection='lcc',lat_1=32,lat_2=45,lon_0=-95)
 
 map.drawstates()
+map.drawcountries()
+map.drawcoastlines()
 
 # Get the location of each city and plot it
 for (city, latitude, longitude) in cities:
